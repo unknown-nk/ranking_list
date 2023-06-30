e6630d565e24d93b6a91c43997ca27709394f92d
nu pan <135819902+unknown-nk@users.noreply.github.com>
2023-06-30 18:41:50 +0800

    feat: layout v1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RankComponent } from './rank/rank.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RankComponent
   ],
   imports: [
     BrowserModule,
